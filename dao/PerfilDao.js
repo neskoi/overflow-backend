bae3f4173d6ba48e9eecebe2bb03707e1b757c72
Carlos Magno Marcondes <you@example.com>
2020-10-21 00:48:22 -0300

    functions of perfil ok

--- dao/PerfilDao.js
+++ dao/PerfilDao.js
@@ -5,7 +5,18 @@ class PerfilDao {
     this.db = connection;
   }
   async listPerfil(id_usuario) {
-    const perfil = await db('users').where('id', id_usuario).select('*');
+    const perfil = await db('users').where('id', id_usuario).select('*')
+
+    for(const asks of perfil){
+        const total_asks = await db('asks').where('fk_asks_users', id_usuario).select('*')
+        asks['posts'] = total_asks;
+    }
+
+    for(const answers of perfil){
+        const total_answers = await db('answers').where('fk_answers_users', id_usuario).select('*')
+        answers['answers'] = total_answers;
+    }
+
     return perfil;
   }
 }
